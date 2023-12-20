30185986d3118e1c0f72a2ed06dd2bbd827695df
Vovede <zarubin.fr@gmail.com>
2023-12-20 12:56:01 +0300

    DataBase

--- bd.py
+++ bd.py
@@ -1,17 +1,46 @@
 import sqlite3
 
-conn = sqlite3.connect("bd.db")
+conn = sqlite3.connect('bd.db')
 cursor = conn.cursor()
-
+# станция, год, месяц, день, час, напр ветра, осадки, темпер-ра, влажность
 cursor.execute("""CREATE TABLE IF NOT EXISTS stations(
-    idstations INT PRIMARY KEY,
-    stname TEXT,
-    year TEXT,
-    month TEXT,
-    day TEXT,
-    hour TEXT,
-    dirwind TEXT,
-    precipit REAL,
-    temperature REAL,
-    humidity REAL);""")
+                idstations INT PRIMARY KEY,
+                stname TEXT,
+                year INT,
+                month INT,
+                day INT,
+                hour INT,
+                dirwind TEXT,
+                precipit REAL,
+                temperature REAL,
+                humidity REAL);"""
+               )
 conn.commit()
+
+# Добавление записей в БД:
+"""
+INSERT INTO stations (
+                         idstations,
+                         stname,
+                         year,
+                         month,
+                         day,
+                         hour,
+                         dirwind,
+                         precipit,
+                         temperature,
+                         humidity
+                     )
+                     VALUES (
+                         2,
+                         '20046',
+                         '1966',
+                         '12',
+                         '31',
+                         '23',
+                         '3',
+                         '10.0',
+                         '-30.0',
+                         '70.0'
+                     );
+"""
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import matplotlib.pyplot as plt
+import db_add
 
-f = open("20046.dat", "r")
+f = open('20046.dat', 'r')
 data = []
+counter = 10000000
 for s in f.readlines():
     temp = s.split()
-                #год, месяц, день, час, напр ветра, осадки, темп-ра, влажность
+                # станция, год, месяц, день, час, напр ветра, осадки, темпер-ра, влажность
     data.append([temp[0], temp[5], temp[6], temp[7], temp[10], float(temp[39]), float(temp[47]), float(temp[-31]), float(temp[-15])])
-#print(*data[:10], sep="\n")
+    db_add.add_inf(counter, temp)
+    counter += 1
+# print(*data[:10], sep="\n")
 
 x_range = list(map(int, input().split()))
 
 x = range(len(data[x_range[0]:x_range[1]]))
 y = [i[-2] for i in data[x_range[0]:x_range[1]]]
 
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(7, 7))
 plt.subplot(3, 1, 1)
-plt.title("y = x")
-plt.xlabel("время")
-plt.ylabel("температура")
+plt.title('Температура')
+plt.xlabel('Время')
+plt.ylabel('Температура')
 plt.grid()
 plt.plot(x, y, "g")
-
 plt.show()
-
